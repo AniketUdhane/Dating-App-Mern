bc14a036643eb4fd0a3f6985f96377ab4724b5ad
AniketUdhane <aniketudhane10@gmail.com>
2025-03-01 17:47:10 +0530

    Converted submodule <folder-name> to a normal folder

---
+++ dating-app-frontend/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+import IconButton from '@mui/material/IconButton';
+import PersonIcon from '@mui/icons-material/Person';
+import ForumIcon from '@mui/icons-material/Forum';
+
+export default function Header() {
+  return (
+    <div className="header">
+      <IconButton>
+        <PersonIcon fontSize="large" className="header_icon" />
+      </IconButton>
+      <img className="header_logo" src="logo192.png" alt="header" />
+      <IconButton>
+        <ForumIcon fontSize="large" className="header_icon" />
+      </IconButton>
+    </div>
+  );
+}
